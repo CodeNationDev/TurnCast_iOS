16b951e270a0468925b586573a71ca8f2e14f73d
David Martín Sáiz <eldeividadmin@macbook-pro-de-david.local>
2018-03-20 07:28:35 +0100

    Initial Commit

---
+++ Turncast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Turncast
+//
+//  Created by David Martín Sáiz on 20/3/18.
+//  Copyright © 2018 CodeNationDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
